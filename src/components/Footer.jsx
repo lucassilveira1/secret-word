990dfaa93df647c50cdf1855c7ba367271dc9015
Lucas <lucas.silveira0516@gmail.com>
2024-10-02 14:25:18 -0300

    fix: css>> responsive and new feat>> adding a footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+    return (
+        <>
+            <div className="footer">
+                <p>₢ Lucas Silveira 2023 | Todos os Direitos Reservados</p>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
